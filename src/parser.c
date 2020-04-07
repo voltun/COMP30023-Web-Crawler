d26fe13caf98bb1a3464fe249b70f7cdb0c24fbc
Nicholas Wong <chungw4@student.unimelb.edu.au>
2020-04-07 08:11:24 +0000

    Temporarily removed parser.c fnc

--- src/parser.c
+++ src/parser.c
@@ -5,7 +5,7 @@
 Finds all valid html redirects in the given text
 Returns a 2D char array of found url
 */
-char** parse_html(char* text)
-{
-    return char**;
-}
+// char** parse_html(char* text)
+// {
+//     return ;
+// }
